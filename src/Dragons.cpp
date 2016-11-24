d23d43d25c30464813e7ac8411aff0007835955a
Марат Зайнуллин <marat.zainullin.96@gmail.com>
2016-11-24 09:58:19 +0300

    декларации всех классов

--- src/Dragons.cpp
+++ src/Dragons.cpp
@@ -1,5 +1,49 @@
-//
-// Created by Marat on 24.11.2016.
-//
-
 #include "Dragons.h"
+
+
+
+
+/***********AdditionDragon*****************************************************/
+std::string AdditionDragon::generateQuestion() {
+    return std::__cxx11::string();
+}
+
+AdditionDragon::AdditionDragon(int health,
+                               int attackForce,
+                               std::string name,
+                               int killPoints)
+        : Enemy(health, attackForce, name, killPoints) {
+
+}
+
+
+
+
+/***********SubtractionDragon**************************************************/
+std::string SubtractionDragon::generateQuestion() {
+    return std::__cxx11::string();
+}
+
+SubtractionDragon::SubtractionDragon(int health,
+                                     int attackForce,
+                                     std::string name,
+                                     int killPoints)
+        : Enemy(health, attackForce, name, killPoints) {
+
+}
+
+
+
+
+/***********MultiplicationDragon***********************************************/
+std::string MultiplicationDragon::generateQuestion() {
+    return std::__cxx11::string();
+}
+
+MultiplicationDragon::MultiplicationDragon(int health,
+                                           int attackForce,
+                                           std::string name,
+                                           int killPoints)
+        : Enemy(health, attackForce, name, killPoints) {
+
+}
--- src/Dragons.h
+++ src/Dragons.h
@@ -4,27 +4,35 @@
 #include "Enemy.h"
 
 class AdditionDragon : public Enemy {
-private:
-    int default_health_;
-    int default_attackForce_;
-    std::string default_name_;
-    int default_killPoints_;
-
 public:
-    AdditionDragon(int health, int attackForce, std::string name, int killPoints);
+    AdditionDragon(int health,
+                   int attackForce,
+                   std::string name,
+                   int killPoints);
+
+    std::string generateQuestion();
 };
 
 
 class SubtractionDragon : public Enemy {
 public:
-    SubtractionDragon();
+    SubtractionDragon(int health,
+                      int attackForce,
+                      std::string name,
+                      int killPoints);
+
+    std::string generateQuestion();
 };
 
 
 class MultiplicationDragon : public Enemy {
 public:
-    MultiplicationDragon();
-};
+    MultiplicationDragon(int health,
+                         int attackForce,
+                         std::string name,
+                         int killPoints);
 
+    std::string generateQuestion();
+};
 
 #endif //ARITHMETICAL_DRAGONS_DRAGONS_H
